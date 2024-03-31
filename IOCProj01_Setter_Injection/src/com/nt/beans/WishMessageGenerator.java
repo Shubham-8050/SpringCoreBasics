e13e7779d1d6214a6adf7f855e128340823ba60b
HP <HP@DESKTOP-LEJI3U9>
2024-03-31 15:26:54 +0530

    Added the final code

--- IOCProj01_Setter_Injection/src/com/nt/beans/WishMessageGenerator.java
+++ IOCProj01_Setter_Injection/src/com/nt/beans/WishMessageGenerator.java
@@ -1,5 +1,31 @@
 package com.nt.beans;
 
-public class WishMessageGenerator {
+import java.util.Date;
 
+public class WishMessageGenerator {
+	
+	//Setter Injection
+	public Date date;
+
+	//to check object
+	public WishMessageGenerator() {
+		System.out.println("## Constructor called ##");
+	}
+
+	//Setter method
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getWishGenerator(String name) {
+		int hour = date.getHours();
+		if(hour<12) {
+			return "good morning "+name;
+		}else if(hour<16) {
+			return "good afternoon "+name;
+		}else {
+			return "Good night "+name;
+		}
+	}
+	
 }
--- IOCProj01_Setter_Injection/src/com/nt/test/SetterInjectionTest.java
+++ IOCProj01_Setter_Injection/src/com/nt/test/SetterInjectionTest.java
@@ -1,10 +1,23 @@
 package com.nt.test;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+import com.nt.beans.WishMessageGenerator;
+
 public class SetterInjectionTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//locate and hold the spring bean configuration files
+		FileSystemResource fsr = new FileSystemResource("src/com/nt/config/applicationContext.xml");
+		//create XmlBeanFactory container by passing spring bean configuration file
+		XmlBeanFactory factory = new XmlBeanFactory(fsr);
+		//create target class object
+		Object obj = factory.getBean("wmg");
+		//Typecast to its original Type
+		WishMessageGenerator gen = (WishMessageGenerator)obj;
+		//call the method
+		String message = gen.getWishGenerator("Shubham");
+		System.out.println(message);
 	}
-
 }
